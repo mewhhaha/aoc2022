d15b3d77432a3393ecb8aa0d4d0b6fba5a17a035
Jacob Torrång <kopatheonlyone@hotmail.com>
2022-12-16 21:05:03 +0100

    Add day15

---
+++ day15/examples/part1.rs
@@ -0,0 +1,73 @@
+#![feature(slice_group_by)]
+
+use parse_display::{Display, FromStr};
+use std::collections::HashSet;
+use std::io;
+use std::ops::{Add, Sub};
+
+#[derive(Display, FromStr, PartialEq, Debug, Clone, Eq, Hash, Copy)]
+#[display("x={x}, y={y}")]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+#[derive(Display, FromStr, PartialEq, Debug, Clone, Eq, Hash, Copy)]
+#[display("Sensor at {sensor}: closest beacon is at {beacon}")]
+struct Sensor {
+    sensor: Point,
+    beacon: Point,
+}
+
+fn main() {
+    let sensors = io::stdin()
+        .lines()
+        .filter_map(|line| line.unwrap().parse::<Sensor>().ok())
+        .collect::<Vec<_>>();
+    let row_y = 2000000;
+
+    let mut no_beacon = HashSet::<Point>::new();
+
+    for Sensor { sensor, beacon } in sensors.iter() {
+        let d = distance(&sensor, &beacon);
+
+        for x in (sensor.x - d)..=(sensor.x + d) {
+            let p = Point { x, y: row_y };
+            if distance(&sensor, &p) <= d {
+                no_beacon.insert(p);
+            }
+        }
+    }
+
+    for Sensor { beacon, .. } in sensors.iter() {
+        no_beacon.remove(beacon);
+    }
+
+    println!("{:?}", no_beacon.len());
+}
+
+fn distance(a: &Point, b: &Point) -> i32 {
+    (a.x - b.x).abs() + (a.y - b.y).abs()
+}
---
+++ day15/examples/part2.rs
@@ -0,0 +1,103 @@
+#![feature(slice_group_by)]
+
+use parse_display::{Display, FromStr};
+use std::collections::HashSet;
+use std::io;
+use std::ops::{Add, Sub};
+
+#[derive(Display, FromStr, PartialEq, Debug, Clone, Eq, Hash, Copy)]
+#[display("x={x}, y={y}")]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+#[derive(Display, FromStr, PartialEq, Debug, Clone, Eq, Hash, Copy)]
+#[display("Sensor at {sensor}: closest beacon is at {beacon}")]
+struct Sensor {
+    sensor: Point,
+    beacon: Point,
+}
+
+fn main() {
+    let sensors = io::stdin()
+        .lines()
+        .filter_map(|line| line.unwrap().parse::<Sensor>().ok())
+        .collect::<Vec<_>>();
+
+    let limit = 4000000;
+
+    let mut potential_positions = HashSet::<Point>::new();
+
+    for Sensor { sensor, beacon } in sensors.iter() {
+        let d = distance(&sensor, &beacon) + 1; // The empty one has to be one distance from the edge
+
+        let a = (sensor.x - d..sensor.x)
+            .zip(sensor.y..sensor.y + d)
+            .map(|(x, y)| Point { x, y })
+            .collect::<Vec<_>>();
+        let b = rotate(&a);
+        let c = rotate(&b);
+        let d = rotate(&c);
+
+        potential_positions.extend(a.iter());
+        potential_positions.extend(b.iter());
+        potential_positions.extend(d.iter());
+        potential_positions.extend(c.iter());
+    }
+
+    let result = potential_positions
+        .iter()
+        .filter(|p| {
+            if p.x < 0 || p.x > limit || p.y < 0 || p.y > limit {
+                return false;
+            }
+
+            for Sensor { sensor, beacon } in sensors.iter() {
+                let d = distance(&sensor, &beacon);
+                if distance(p, sensor) <= d {
+                    return false;
+                }
+            }
+            return true;
+        })
+        .next()
+        .map(|Point { x, y }| (x * 4000000) + y);
+
+    println!("{:?}", result);
+}
+
+fn rotate(vec: &Vec<Point>) -> Vec<Point> {
+    vec.iter()
+        .map(|Point { x, y }| Point {
+            x: -y,
+            y: x.clone(),
+        })
+        .collect::<Vec<_>>()
+}
+
+fn distance(a: &Point, b: &Point) -> i64 {
+    (a.x - b.x).abs() + (a.y - b.y).abs()
+}
